0c5dcf32193fcfda2c1cb5d86e3596408528767e
Ondřej Šplíchal <ondrej.splichal@gmail.com>
2024-02-28 16:50:37 +0100

    read CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        panic!("Invalid arguments. Usage: vincrementer version_string");
+    }
+    let input_version: String = args[1].clone();
+    println!("{input_version}");
 }
